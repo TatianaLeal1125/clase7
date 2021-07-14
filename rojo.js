4131c547013a29e6543d1d19a9720ca43f1e20db
TatianaLeal1125 <mileydy.1125@gmail.com>
2021-07-14 13:26:04 -0500

    Imprime por consola la función suma

--- rojo.js
+++ rojo.js
@@ -4,7 +4,8 @@ console.log("Rojo");
 let suma = (num1, num2) => num1 + num2;
 console.log(suma(2,3));
 
-
 //Esta línea la agregó Tatiana Leal 
 console.log("Este es el color rojo");
 
+//Probando conflictos. Tatiana Leal
+console.log(suma(2165,874));
